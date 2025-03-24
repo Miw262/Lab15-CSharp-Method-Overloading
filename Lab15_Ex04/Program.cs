c18c21a1e89e2212b8b0de90c5b7a4fee352bc23
Miw262 <chauat246@gmail.com>
2025-03-24 18:17:40 +0700

    1

---
+++ Lab15_Ex04/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;  // เพิ่มบรรทัดนี้
+
+var b = new Base();
+var d1 = new Derived_1();
+var d2 = new Derived_2();
+
+b.A();
+d1.A();
+d2.A();
+
+class Base
+{
+    public virtual void A()
+    {
+        WriteLine("Base.A()");  // ใช้ WriteLine ได้โดยไม่ต้องระบุ Console
+    }
+}
+
+class Derived_1 : Base
+{
+    public override void A()
+    {
+        WriteLine("Derived_1.A()");
+    }
+}
+
+class Derived_2 : Derived_1
+{
+    public override void A()
+    {
+        WriteLine("Derived_2.A()");
+    }
+}
---
+++ Lab15_Ex07/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+Derived_2 d2 = new Derived_2();
+Base b = (Base) d2;
+Derived_1 d1 = (Derived_1) d2;
+
+b.A();
+d1.A();
+d2.A();
+
+class Base
+{
+    public virtual void A()
+    {
+        WriteLine("Base.A()");
+    }
+}
+
+class Derived_1 : Base
+{
+    // ลบคำว่า 'sealed' ออก เพื่อให้สามารถ override ใน Derived_2 ได้
+    public override void A()
+    {
+        WriteLine("Derived_1.A()");
+    }
+}
+
+class Derived_2 : Derived_1
+{
+    // ตอนนี้สามารถ override A ได้
+    public override void A()
+    {
+        WriteLine("Derived_2.A()");
+    }
+}
